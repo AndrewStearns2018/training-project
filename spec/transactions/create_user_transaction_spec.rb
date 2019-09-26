836f7bdbc49dd20cc068dead0a428fb76b80d688
Andrew Stearns <andrew.stearns@sciencespo.fr>
2019-09-26 18:00:00 +0200

    Improved transaction tests

--- spec/transactions/create_user_transaction_spec.rb
+++ spec/transactions/create_user_transaction_spec.rb
@@ -4,23 +4,32 @@ require 'rails_helper'
 RSpec.describe CreateUserTransaction do
   context 'CreateUserTransaction creates User and sends email.' do
     let(:user) { attributes_for(:user) }
-    it 'should create a valid user and send email' do
-      expect(CreateUserTransaction.new.call(user_params: user)).to be_success
+    subject { CreateUserTransaction.new.call(user_params: user) }
+    it 'should return success' do
+      expect(subject).to be_success
+    end
+
+    it 'should create a valid user' do
+      expect(subject.success[:user]).to be_an_instance_of(User)
+    end
+
+    it 'should send a welcome email' do
+      expect { subject }.to change { ActionMailer::Base.deliveries.count }
     end
 
     it 'should not create a user without first_name.' do
       user[:first_name] = nil
-      expect(CreateUserTransaction.new.call(user_params: user)).to be_failure
+      expect(subject).to be_failure
     end
 
     it 'should not create a user without last_name' do
       user[:last_name] = nil
-      expect(CreateUserTransaction.new.call(user_params: user)).to be_failure
+      expect(subject).to be_failure
     end
 
     it 'should not create a user without date_of_birth' do
       user[:date_of_birth] = nil
-      expect(CreateUserTransaction.new.call(user_params: user)).to be_failure
+      expect(subject).to be_failure
     end
   end
 end
