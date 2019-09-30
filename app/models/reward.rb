721e1223d89f7f8da069999b58a8a5c0d4064a3b
Andrew Stearns <andrew.stearns@sciencespo.fr>
2019-09-30 15:54:15 +0200

    Admin project show update and contribution index

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,6 +1,6 @@
 class Reward < ApplicationRecord
   belongs_to :project
-  has_many :contributions
+  has_many :contributions, dependent: :destroy
 
   validates :name, presence: true
   validates :units, presence: true, numericality: { only_integer: true }
